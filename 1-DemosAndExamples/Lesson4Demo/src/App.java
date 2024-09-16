3ea9b9d005544a67b6a045df0f3eba83682b6833
Ri7k <richmeana@gmail.com>
2024-09-16 09:28:36 -0600

    kawaii bunny

---
+++ 1-DemosAndExamples/Lesson4Demo/src/App.java
@@ -0,0 +1,16 @@
+// Project: Lesson 4 Demo
+// Name:Richmond
+// Date: 13 Sept 2024
+// Description: Going over comments and special characters
+/*testing sopurce block
+ * ssldfsldkfjs
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("How is your day going");
+        System.out.println("It\'s \"cloudy\" with a chance of meatballs today!");
+    }
+}
---
+++ A4 ASCII Art/src/App.java
@@ -0,0 +1,13 @@
+/* Richmond
+ * Mr Fox
+ * ASCII PROJECT
+ * FiSH
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("(\\_ _/)");
+        System.out.println("(='.'=)");
+        System.out.println("(\")_(\")");
+    }
+}
