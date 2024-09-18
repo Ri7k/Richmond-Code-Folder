919572f43fccffd5977249ef354dea7ae62e68ac
Ri7k <richmeana@gmail.com>
2024-09-18 08:55:39 -0600

    Lesson Variables

---
+++ 1-DemosAndExamples/LessonVariables/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println((double)1/3);
+        // System.out.println(1.0/3);
+        // System.out.println(1/3.0);
+        // System.out.println(1.0/3.0);
+
+        // int myNum  = 2147483647;
+        // System.out.println(myNum);
+        // myNum = myNum + 1;
+        // System.out.println(myNum);
+        // System.out.println(Integer.MAX_VALUE);
+
+        double num = Math.random();
+        num = num*10 ;
+        System.out.println(num);
+
+    }
+
+}
+
