8cf5921c5b41d6e9b716fc2f3eca3325b702864a
Ri7k <richmeana@gmail.com>
2024-09-19 09:32:24 -0600

    A7 String basics

---
+++ A7 Stringy2/src/App.java
@@ -0,0 +1,15 @@
+// Richmond Meana
+// Mr Fox
+// String Basics
+// 18 Sept 2024
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 = "I am very happy!";
+        //String target1 = "very";
+        String pos1 = message1.substring(0, 4) + message1.substring(9, message1.length());
+        System.out.println(pos1);
+    }
+}
