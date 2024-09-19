16f95c6e9f8bcf5f030d9c34958b85098d37c21c
Ri7k <richmeana@gmail.com>
2024-09-19 09:45:05 -0600

    String A7 pt2

--- A7 Stringy2/src/App.java
+++ A7 Stringy2/src/App.java
@@ -3,13 +3,20 @@
 // String Basics
 // 18 Sept 2024
 
-
-
 public class App {
     public static void main(String[] args) throws Exception {
+        // PART ONE
         String message1 = "I am very happy!";
         //String target1 = "very";
         String pos1 = message1.substring(0, 4) + message1.substring(9, message1.length());
         System.out.println(pos1);
+
+        //PART TWO 
+        String msg2 = "That was great - lol." ; 
+        //String target2 = "lol" ; 
+        String msgFull = "laugh out loud.";
+        String pos2 = msg2.substring(0, 16) + msg2.substring(16, 17) ;
+        System.out.println(pos2 + msgFull);
+
     }
 }
