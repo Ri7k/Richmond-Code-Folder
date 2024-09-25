6b0dbf71161502d9caa06a973701626c1048d153
Ri7k <richmeana@gmail.com>
2024-09-25 09:36:30 -0600

    a8b passing methods both files
    turkey is the methods file :eyeglasses:
    app file is the running file

---
+++ A8b Methods/src/App.java
@@ -0,0 +1,15 @@
+// Richmond Meana
+// Mr. Fox 
+// 25 Sept 2024
+// Passing Methods 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+    Turkey riyk = new Turkey();
+
+        riyk.pressE();
+        riyk.holdShift();
+
+   }
+}
---
+++ A8b Methods/src/Turkey.java
@@ -0,0 +1,16 @@
+// Richmond Meana
+// Mr. Fox 
+// 25 Sept 2024
+// Passing Methods | part 2, methods file
+
+import static java.lang.System.*;
+
+public class Turkey
+{
+   public void pressE() {
+      out.println("rolling forward");
+   }   
+   public void holdShift() {
+      out.println("Running fast");
+   }
+}
