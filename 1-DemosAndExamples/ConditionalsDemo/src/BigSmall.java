8ede88431ae94438039ac0ca4bfdd82121a73192
Ri7k <richmeana@gmail.com>
2024-10-03 09:09:08 -0600

    big or small

---
+++ 1-DemosAndExamples/ConditionalsDemo/src/BigSmall.java
@@ -0,0 +1,10 @@
+public class BigSmall {
+    int x = 2;
+    int y = 3;
+
+    if ( !(x > 0) || !(y < 2) ) {
+        System.out.println("wear a coat");
+        System.out.println("wear gloves");
+    }
+    System.out.println("Bye");
+}
---
+++ 1-DemosAndExamples/Lesson6 Demo/src/Turkey.java
@@ -0,0 +1,9 @@
+public class Turkey {
+    public void speak() {
+        System.out.println("yare yare");
+    }
+    public void summon() {
+        System.out.println("ZA WORLDU");
+        
+    }
+}
