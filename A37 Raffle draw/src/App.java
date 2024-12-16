d118bb58b854613dea6c8efd0adf8c88b8d01c94
Ri7k <richmeana@gmail.com>
2024-12-16 09:36:21 -0600

    a37(not done)
    not done

---
+++ A37 Raffle draw/src/App.java
@@ -0,0 +1,15 @@
+// Richmond Meana
+// Mr Fox
+// 12/11/2024
+// A37 - Reverse Raffle draw 
+
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] names = new int[10];
+        
+    }
+}
+
+
+//future ricmond: look at array lists demo to add names to hatArrayList
